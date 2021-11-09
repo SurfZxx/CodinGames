6f41c478008546507e569fbe715c2410c2054b61
SurfZxx <hugo.sousa.sa@hotmail.com>
2021-11-09 18:18:31 +0000

    bernardo idea

--- Temperatures/src/App.java
+++ Temperatures/src/App.java
@@ -20,26 +20,35 @@ public class App {
             Scanner scanner = new Scanner(System.in);
             int n = scanner.nextInt(); // the number of temperatures to analyse
             int[] temps = new int[n];
+            int almostZero = 5526;
             for (int i = 0; i < n; i++) {
                 temps[i] = scanner.nextInt();//a temperature expressed as an integer ranging from -273 to 5526
             }
-    
-            int[] temp2 = new int[n];
-            int counter = 0;
-            for (int i = 0; i < n; i++) {
-                if (temps[i] < 0) {
-                    temp2[i] = temps[i] * (-1);
-                    counter++;
-                } else {
-                    temp2[i] = temps[i];
-                    counter++;
+            for (int i = 1; i < n; i++) {
+                if (Math.abs(temps[i]) < Math.abs(temps[i-1]) ) {
+                    almostZero = temps[i];
+                } else if (Math.abs(temps[i]) > Math.abs(temps[i-1]) && almostZero > temps[i-1]) {
+                    almostZero = temps[i-1];
                 }
             }
-            Arrays.sort(temp2);
-            reverse(temp2);
-            int almostZero = temp2[counter-1];
-            
+
     
+            // int[] temp2 = new int[n];
+            // int counter = 0;
+            // for (int i = 0; i < n; i++) {
+            //     if (temps[i] < 0) {
+            //         temp2[i] = temps[i] * (-1);
+            //         counter++;
+            //     } else {
+            //         temp2[i] = temps[i];
+            //         counter++;
+            //     }
+            // }
+            // Arrays.sort(temp2);
+            // reverse(temp2);
+            // int almostZero = temp2[counter-1];
+            
+            
             
     /*
             int[] temp2 = new int[n];
